9da4ccc6643327c973874f0efd686621f8b12eb9
Simon <57576661+simon-stentoft@users.noreply.github.com>
2021-09-16 11:18:23 +0200

    Update Main.java
    Ændringer du måske kunne lave - Code review

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,9 +26,12 @@ public class Main {
         int seconds_int;
         seconds_int = (int) seconds;
 
+	 // Dette kan laves på en enkelt linje, ville se bedre ud mener jeg :). System.out.printf("Number of hours: = %d, Number of minutes: = %d, Number of seconds = %d",hours_int,minutes_int,seconds_int);  
         System.out.printf("Number of hours: = %d \n",hours_int);
         System.out.printf("Number of minutes: = %d \n",minutes_int);
         System.out.printf("Number of seconds: = %d \n",seconds_int);
+	    
+	    
         /* It should (1) prompt the user for input, (2) read an integer from the keyboard,
         (3) calculate the result, and (4) use printf to display the output.
         For example, "5000 seconds = 1 hours, 23 minutes, and 20 seconds". */
